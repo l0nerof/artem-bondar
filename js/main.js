848d5662f3f94bd96bf175ae9deab6f55f4c2067
Volodymyr Bondar <2005volka@gmail.com>
2023-08-17 21:16:59 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
+AOS.init({
+  duration: 1000,
+  easing: "ease",
+  once: true,
+});
+
 const faqs = document.querySelectorAll(".faq");
 
 faqs.forEach((faq) => {
   faq.addEventListener("click", () => {
     faq.classList.toggle("active");
   });
-});
-
-AOS.init({
-  duration: 1000,
-  easing: "ease",
-  once: true,
 });
